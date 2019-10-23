c2af4c2093df71075a29f7abc76fd8075c69c26b
git <alberto.colella@codever.it>
2019-10-23 09:42:53 +0200

    v1.0.1 added ignore and dir parameters

--- README.md
+++ README.md
@@ -2,13 +2,26 @@
 
 [![npm (scoped)](https://img.shields.io/npm/v/@codever/npm-execute-submodules.svg?style=plastic)](https://www.npmjs.com/package/@codever/npm-execute-submodules)
 
-Executes a command through all the NPM modules in `node_modules` folder.
+Executes an npm script through all the NPM modules in `node_modules` folder.
 
 ## How does it work
 
-The provided command will be executed for every submodule inside `node_modules` folder.
+The provided script will be executed for every `package.json` file that has that script implemented inside `node_modules` folder.
 It is supposed to fit well with complex projects having custom submodules as dependencies.
 If those submodules have an updated version with new migrations or new tests, you can make it all run with a single command.
+
+### Why I should need it
+
+Every `package.json` file can have a `scripts` section like
+
+```
+  "scripts": {
+    "test": "jest mytest --notify"
+  },
+```
+
+if you need to execute `npm run test` on all (or part) of our submodules, you could use `execute-submodules test`
+
 
 ### Why not using npm explore?
 
@@ -25,8 +38,14 @@ npm install @codever/execute-submodules --save-dev
 ## Usage
 
 ```
-execute-submodules [command]
+execute-submodules [command] [--dir [directory]] [--ignore [regexp]] [--verbose]
 ```
+
+where
+
+ * `--dir` sets a specific subdirectory where the packages. Default value is `node_modules`, but using local modules could need a different directory
+ * `--ignore` sets a regular expression pattern to exclude matches
+ * `--verbose` output every module where the npm script is executed
 
 ### Usage with npm
 
@@ -37,7 +56,7 @@ npx execute-submodules [command]
 
 #### Examples
 
-Run all the migration throught all the submodules
+Run all the migration (`npm run migrate`) throught all the submodules
 
 ```
 execute-submodules migrate
--- bin/execute-submodules.js
+++ bin/execute-submodules.js
@@ -4,49 +4,109 @@ const [,, ...args] = process.argv;
 const command = require('meow')
 const glob = require('glob');
 const path = require('path');
-
+const appRoot = require('app-root-path');
+const fs = require('fs');
 
 const cli = command(`
   Usage
     $ execute-submodules <command>
-`)
+  
+  Options
+    --verbose, -v enable verbose mode
+    --dir [directory], -d limit the execution to the given directory
+    --ignore [directory], glob regexp to ignore
+`, {
+	flags: {
+		verbose: {
+			type: 'boolean',
+			alias: 'v'
+    },
+    dir: {
+			type: 'string',
+			alias: 'v'
+		}
+  },
+  input:{
+    command: {
+      type: 'string'
+    }   
+	}
+})
 
 
 if (cli.input.length === 0) {
-  console.error('Missing: command.')
+  console.error('Missing command')
   cli.showHelp(1)
 }
 
 let options = {};
+let processedArgs = processArgs(cli);
+let glob_path = path.join(appRoot.path, processedArgs.dir, "**/package.json");
 options.cwd = process.cwd();
 options.root = path.resolve(options.cwd, "/");
-
-glob("**/package.json", options, function (er, files) {
-  let commandLine = args.join(' ');
+options.ignore = processedArgs.ignore;
+glob(glob_path, options, function (err, files) {
+  console.log('[npm execute submodules] Starting execution for command:', processedArgs.commandLine);
   files.map((file)=>{
-    try {
-      let path = file.replace('package.json', '');
-        if(!path || path.length==0){
-            path = ".";
+    let opts = {};
+    try {      
+      let packageJson = JSON.parse(fs.readFileSync(file, 'utf8'));
+      let scripts = packageJson.scripts;
+      if(!scripts || !scripts.hasOwnProperty(processedArgs.script)){
+        return null;
+      }
+      let path = file.replace('/package.json', '');
+      path = path.replace('package.json', '');
+      if(!path || path.length==0){
+        path = ".";
+      }      
+      opts.cwd = path;
+      opts.stdio = 'inherit';
+      opts.encoding = 'utf-8';
+      if(processedArgs.verbose){
+        console.log('[npm execute submodules] Executing "%s" in %s', processedArgs.commandLine, path);
+      }
+      let res = spawnSync(processedArgs.command, processedArgs.args, opts);
+      if(res.error && res.error.toString().length>0){
+        console.log('[npm execute submodules] ERROR:', res.error.toString());
+      } else {
+        if(processedArgs.verbose){
+          console.log('[npm execute submodules] Executed "%s" in %s with success.', processedArgs.commandLine, path);
         }
-        let opts = {};
-        opts.cwd = path;
-        let res = spawnSync(args[0], args.slice(1), opts);
-        if(res.sterr && res.sterr.toString().length>0){
-          console.log('Executing "%s" in "%s"...', commandLine, path);
-          console.log('ERROR:', res.stderr.toString());
-        } else if(res.stdout && res.stout.toString().length>0){
-          console.log('Executing "%s" in "%s"...', commandLine, path);
-          console.log('RESULT:', res.stdout.toString());
-        }
+      }
     } catch(e) {
-      console.error('Error on executing "%s" in "%s": %s', commandLine, path, e);
+      console.error('[npm execute submodules] Error on executing "%s" in %j: %s', processedArgs.commandLine, opts.cwd || path, e);
       process.exit(1);
     }
   });
   process.exit(0);
-})
-
-process.on('exit', function() {
-  console.log('END');
 });
+
+process.on('exit', function(code) {
+  console.log('[npm execute submodules] end of execution.');
+});
+
+function processArgs(cliArgs){
+  let script = cliArgs.input[0];
+  var result = {
+    command: 'npm',
+    commandLine: 'npm run '+script,
+    verbose: false,
+    args: ['run', script],
+    script: script,
+    dir: 'node_modules',
+    ignore: null
+  };
+  if(cliArgs.flags){
+    if(cliArgs.flags.hasOwnProperty('verbose') || cliArgs.flags.hasOwnProperty('v')){
+      result.verbose=cliArgs.flags.verbose || cliArgs.flags.v;
+    }
+    if(cliArgs.flags.hasOwnProperty('dir')){
+      result.dir=cliArgs.flags.dir;
+    }
+    if(cliArgs.flags.hasOwnProperty('ignore')){
+      result.ignore=cliArgs.flags.ignore;
+    }
+  }
+  return result;
+}
